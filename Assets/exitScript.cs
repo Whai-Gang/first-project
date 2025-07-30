4c68dd99f4c9b5557bf515580735849c8f03f3f0
Callum Begley <cfbegley7@gmail.com>
2025-07-30 13:56:24 +1200

    door opens when levle complete

--- Assets/exitScript.cs
+++ Assets/exitScript.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class exitScript : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        // Get the sprite renderer and store original color
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            originalColor = spriteRenderer.color;
+        }
     }
 
     // Update is called once per frame
@@ -24,6 +32,13 @@ public class exitScript : MonoBehaviour
             if (coinScript.hasCollectedCoin == true)
             {
                 Debug.Log("Level complete!");
+                
+                // Change sprite color to black
+                if (spriteRenderer != null)
+                {
+                    spriteRenderer.color = Color.black;
+                }
+                
                 // Add level completion logic here
                 // Example: SceneManager.LoadScene("NextLevel");
             }
